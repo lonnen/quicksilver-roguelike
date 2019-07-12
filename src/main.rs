ca143ff5cbfd4110feb2c27b460ee754321feb81
lonnen <chris.lonnen@gmail.com>
2019-07-11 18:03:20 -0700

    add the mononoki font asset
    this adds a static asset, loads it on start, and renders both the game title and font credit. nothing is drawing yet.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 use quicksilver::{
     geom::Vector,
-    lifecycle::{run, Settings, State, Window},
+    graphics::{Color, Font, FontStyle, Image},
+    lifecycle::{run, Asset, Settings, State, Window},
+    Future,
     Result,
 };
 
-struct Game;
+struct Game {
+    title: Asset<Image>,
+    mononoki_font_info: Asset<Image>,
+}
 
 impl State for Game {
     /// load assets and initialize
     fn new() -> Result<Self> {
-        Ok(Self)
+        let font_mononoki = "mononoki-Regular.ttf";
+
+        let title = Asset::new(Font::load(font_mononoki).and_then(|font| {
+            font.render("QSR", &FontStyle::new(72.0, Color::BLACK))
+        }));
+
+        let mononoki_font_info = Asset::new(Font::load(font_mononoki).and_then(|font| {
+            font.render(
+                "Mononoki font by Matthias Tellen, terms: SIL Open Font License 1.1",
+                &FontStyle::new(20.0, Color::BLACK))
+        }));
+
+        Ok(Self {
+            title,
+            mononoki_font_info
+        })
     }
 
     /// process human inputs, update game state
