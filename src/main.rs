15c65562bf136c64a69604568961331540e91132
lonnen <chris.lonnen@gmail.com>
2019-07-11 18:55:28 -0700

    behold, a pair of goblins
    adds a couple of entites, in this case goblins and food

--- src/main.rs
+++ src/main.rs
@@ -104,6 +104,40 @@ struct Entity {
     max_hp:
 }
 
+fn generate_entities() -> Vec<Entity> {
+    vec![
+        Entity {
+            pos: Vector::new(9, 6),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity {
+            pos: Vector::new(2, 4),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity {
+            pos: Vector::new(7, 5),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        },
+        Entity {
+            pos: Vector::new(4, 8),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        },
+    ]
+}
+
+
 fn main() {
     let settings = Settings {
         ..Default::default()
