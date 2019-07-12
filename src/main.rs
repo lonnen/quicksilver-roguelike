d7c6a07d5ce47b012e2a3664e7efa4f83924c024
lonnen <chris.lonnen@gmail.com>
2019-07-11 22:47:31 -0700

    first tileset rendering
    this renders our text into something like a spritesheet, then we loop and render the sub-images to the field.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use quicksilver::{
-    geom::{Shape, Vector},
+    geom::{Rectangle, Shape, Vector},
     graphics::{Background::Img, Color, Font, FontStyle, Image},
     lifecycle::{run, Asset, Settings, State, Window},
     prelude::*,
     Future,
     Result,
 };
+
+use std::collections::HashMap;
 
 struct Game {
     title: Asset<Image>,
@@ -14,6 +16,8 @@ struct Game {
     map: Vec<Tile>,
     entities: Vec<Entity>,
     player_id: usize,
+    tileset: Asset<HashMap<char, Image>>,
+    tile_size_px: Vector,
 }
 
 impl State for Game {
@@ -43,6 +47,23 @@ impl State for Game {
             max_hp: 5,
         });
 
+        let game_glyphs = "#@g.%";
+
+        let tile_size_px = Vector::new(12, 24);
+
+        let tileset = Asset::new(Font::load(font_mononoki).and_then(move |text| {
+            let tiles = text
+                .render(game_glyphs, &FontStyle::new(tile_size_px.y, Color::WHITE))
+                .expect("Could not render the font tileset.");
+            let mut tileset = HashMap::new();
+            for (index, glyph) in game_glyphs.chars().enumerate() {
+                let pos = (index as i32 * tile_size_px.x as i32, 0);
+                let tile = tiles.subimage(Rectangle::new(pos, tile_size_px));
+                tileset.insert(glyph, tile);
+            }
+            Ok(tileset)
+        }));
+
         Ok(Self {
             title,
             mononoki_font_info,
@@ -50,6 +71,8 @@ impl State for Game {
             map,
             entities,
             player_id,
+            tileset,
+            tile_size_px,
         })
     }
 
@@ -79,6 +102,22 @@ impl State for Game {
                     .translate((2, window.screen_size().y as i32 - 60)),
                 Img(&image),
             );
+            Ok(())
+        })?;
+
+        let tile_size_px = self.tile_size_px;
+
+        let (tileset, map) = (&mut self.tileset, &self.map);
+        tileset.execute( |tileset| {
+            for tile in map.iter() {
+                if let Some(image) = tileset.get(&tile.glyph) {
+                    let pos_px = tile.pos.times(tile_size_px);
+                    window.draw(
+                        &Rectangle::new(pos_px, image.area().size()),
+                        Blended(&image, tile.color),
+                    );
+                }
+            }
             Ok(())
         })?;
 
