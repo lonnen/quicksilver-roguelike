6b8922f3cfedf3f82fcc34ed085664bad9bebaf8
lonnen <chris.lonnen@gmail.com>
2019-07-12 00:08:41 -0700

    draw entities, add user input

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,77 @@ use quicksilver::{
 
 use std::collections::HashMap;
 
+
+#[derive(Clone, Debug, PartialEq)]
+struct Tile {
+    pos: Vector,
+    glyph: char,
+    color: Color
+}
+
+fn generate_map(size: Vector) -> Vec<Tile> {
+    let width = size.x as usize;
+    let height = size.y as usize;
+    let mut map = Vec::with_capacity(width * height);
+    for x in 0..width {
+        for y in 0..height {
+            let mut tile = Tile {
+                pos: Vector::new(x as f32, y as f32),
+                glyph: '.',
+                color: Color::BLACK,
+            };
+
+            if x == 0 || x == width - 1 || y == 0 || y == height - 1 {
+                tile.glyph = '#';
+            };
+            map.push(tile);
+        }
+    }
+    map
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct Entity {
+    pos:  Vector,
+    glyph: char,
+    color: Color,
+    hp: i32,
+    max_hp: i32,
+}
+
+fn generate_entities() -> Vec<Entity> {
+    vec![
+        Entity {
+            pos: Vector::new(9, 6),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity {
+            pos: Vector::new(2, 4),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity {
+            pos: Vector::new(7, 5),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        },
+        Entity {
+            pos: Vector::new(4, 8),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        },
+    ]
+}
+
 struct Game {
     title: Asset<Image>,
     mononoki_font_info: Asset<Image>,
@@ -88,6 +159,26 @@ impl State for Game {
 
     /// process human inputs, update game state
     fn update(&mut self, window: &mut Window) -> Result<()> {
+        use ButtonState::*;
+
+        let player = &mut self.entities[self.player_id];
+        if window.keyboard()[Key::Left] == Pressed {
+            player.pos.x -= 1.0;
+        }
+        if window.keyboard()[Key::Right] == Pressed {
+            player.pos.x += 1.0;
+        }
+        if window.keyboard()[Key::Up] == Pressed {
+            player.pos.y -= 1.0;
+        }
+        if window.keyboard()[Key::Down] == Pressed {
+            player.pos.y += 1.0;
+        }
+        
+        if window.keyboard()[Key::Escape].is_down() {
+            window.close();
+        }
+
         Ok(())
     }
 
@@ -126,17 +217,32 @@ impl State for Game {
         })?;
 
         let tile_size_px = self.tile_size_px;
-        let offset_px = Vector::new(50, 50);
+        let offset_px = Vector::new(50, 120);
 
+        // Draw the map
         let (tileset, map) = (&mut self.tileset, &self.map);
-        tileset.execute( |tileset| {
+        tileset.execute(|tileset| {
             for tile in map.iter() {
                 if let Some(image) = tileset.get(&tile.glyph) {
                     let pos_px = tile.pos.times(tile_size_px);
-                    let offset_px = Vector::new(50, 120);
                     window.draw(
                         &Rectangle::new(offset_px + pos_px, image.area().size()),
                         Blended(&image, tile.color),
+                    );
+                }
+            }
+            Ok(())
+        })?;
+
+        // Draw the entities
+        let (tileset, entities) = (&mut self.tileset, &self.entities);
+        tileset.execute(|tileset| {
+            for entity in entities.iter() {
+                if let Some(image) = tileset.get(&entity.glyph) {
+                    let pos_px = offset_px + entity.pos.times(tile_size_px);
+                    window.draw(
+                        &Rectangle::new(pos_px, image.area().size()),
+                        Blended(&image, entity.color),
                     );
                 }
             }
@@ -167,77 +273,6 @@ impl State for Game {
     }
 
 }
-
-#[derive(Clone, Debug, PartialEq)]
-struct Tile {
-    pos: Vector,
-    glyph: char,
-    color: Color
-}
-
-fn generate_map(size: Vector) -> Vec<Tile> {
-    let width = size.x as usize;
-    let height = size.y as usize;
-    let mut map = Vec::with_capacity(width * height);
-    for x in 0..width {
-        for y in 0..height {
-            let mut tile = Tile {
-                pos: Vector::new(x as f32, y as f32),
-                glyph: '.',
-                color: Color::BLACK,
-            };
-
-            if x == 0 || x == width - 1 || y == 0 || y == height - 1 {
-                tile.glyph = '#';
-            };
-            map.push(tile);
-        }
-    }
-    map
-}
-
-#[derive(Clone, Debug, PartialEq)]
-struct Entity {
-    pos:  Vector,
-    glyph: char,
-    color: Color,
-    hp: i32,
-    max_hp: i32,
-}
-
-fn generate_entities() -> Vec<Entity> {
-    vec![
-        Entity {
-            pos: Vector::new(9, 6),
-            glyph: 'g',
-            color: Color::RED,
-            hp: 1,
-            max_hp: 1,
-        },
-        Entity {
-            pos: Vector::new(2, 4),
-            glyph: 'g',
-            color: Color::RED,
-            hp: 1,
-            max_hp: 1,
-        },
-        Entity {
-            pos: Vector::new(7, 5),
-            glyph: '%',
-            color: Color::PURPLE,
-            hp: 0,
-            max_hp: 0,
-        },
-        Entity {
-            pos: Vector::new(4, 8),
-            glyph: '%',
-            color: Color::PURPLE,
-            hp: 0,
-            max_hp: 0,
-        },
-    ]
-}
-
 
 fn main() {
     let settings = Settings {
