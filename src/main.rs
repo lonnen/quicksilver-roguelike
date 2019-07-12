eaf6f376062bb728e3f204da182220414192b741
lonnen <chris.lonnen@gmail.com>
2019-07-11 17:52:46 -0700

    basic game interface
    define the struct and implementation, pulling in some helpful dependencies from quicksilver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use quicksilver::{
+    lifecycle::{State, Window},
+    Result,
+};
+
+struct Game;
+
+impl State for Game {
+    /// load assets and initialize
+    fn new() -> Result<Self> {
+        Ok(Self)
+    }
+
+    /// process human inputs, update game state
+    fn update(&mut self, window: &mut Window) -> Result<()> {
+        Ok(())
+    }
+
+    /// output to the screen
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        Ok(())
+    }
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
